b45d7f5dde250f1cb5594c9585f883e3e81bf4b1
Anna Artemova <annartmva@gmail.com>
2024-04-17 19:22:31 +0400

    Fix ssr error

--- app/hooks/useLocalStorage.ts
+++ app/hooks/useLocalStorage.ts
@@ -3,23 +3,27 @@ import { UrlArray } from "../types";
 
 export function useLocalStorage(key: string, initialValue: unknown) {
   const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      console.log(error);
-      return initialValue;
+    if (typeof window !== "undefined") {
+      try {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+      } catch (error) {
+        console.log(error);
+      }
     }
+    return initialValue;
   });
 
   const setValue = (value: any) => {
-    try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      console.log(error);
+    if (typeof window !== "undefined") {
+      try {
+        const valueToStore =
+          value instanceof Function ? value(storedValue) : value;
+        setStoredValue(valueToStore);
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
